6f674bae7577862e3bc346c594bc784d02a36259
Bharathi <Bharathi@Bharathi-PC>
2016-07-17 20:33:40 -0400

    2nd commit

---
+++ kb/src/main/java/com/bharath/sample/kb/App.java
@@ -0,0 +1,13 @@
+package com.bharath.sample.kb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
